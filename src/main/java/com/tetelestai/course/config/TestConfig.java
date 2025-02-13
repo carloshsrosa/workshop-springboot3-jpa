0a389c028ce22f1c476263217cd2a144cfefab96
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-13 18:25:54 -0300

    Subtotal & Total methods

--- src/main/java/com/tetelestai/course/config/TestConfig.java
+++ src/main/java/com/tetelestai/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.tetelestai.course.entities.Category;
 import com.tetelestai.course.entities.Order;
 import com.tetelestai.course.entities.OrderItem;
+import com.tetelestai.course.entities.Payment;
 import com.tetelestai.course.entities.Product;
 import com.tetelestai.course.entities.User;
 import com.tetelestai.course.enums.OrderStatus;
@@ -73,5 +74,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+
 	}
 }
