b2c0ab5df36449457eb732ab15a2ef3d4d7d0104
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-13 12:00:58 -0300

    Category

---
+++ src/main/java/com/tetelestai/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.tetelestai.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tetelestai.course.entities.Category;
+import com.tetelestai.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> user = repository.findById(id);
+		return user.get();
+	}
+}
